5565565b90e1e69331154a65e4411794a3ccdda9
Goutham-ceo <gouthamceo@icloud.com>
2021-01-19 18:41:07 +0530

    Added PauseMenu

---
+++ FeedMeSushisrc/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu: MonoBehaviour
+{
+  public GameObject PausePanel;
+  public GameObject PauseButton;
+
+  
+ public static bool GameIsPaused = false;
+
+ void Start() {
+  GameIsPaused = false;
+  
+}
+
+ void Update() {
+  if (GameIsPaused)
+  {
+    Pause();
+  }
+  else
+  {
+      Resume();
+  }
+}
+
+
+public void Resume ()
+{
+ PausePanel.SetActive(false);
+ Time.timeScale = 1f;
+ GameIsPaused = false;
+}
+
+public void Pause ()
+{
+  PausePanel.SetActive(true);
+  Time.timeScale = 0f;
+  GameIsPaused = true;
+}
+
+
+
+
+}
+
+
