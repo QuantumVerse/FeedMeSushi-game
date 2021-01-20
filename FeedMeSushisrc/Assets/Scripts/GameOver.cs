600bbd0876e4882b161cc54600e8e06815bbe961
Goutham-ceo <gouthamceo@icloud.com>
2021-01-20 19:35:40 +0530

    GameOverMenu

---
+++ FeedMeSushisrc/Assets/Scripts/GameOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+public GameObject ReplayButton;
+public GameObject MainMenuButton;
+
+    public void OpenMainMenu ()
+{
+  SceneManager.LoadScene(0);
+}
+   
+   public void ReplayGame ()
+   {
+       SceneManager.LoadScene(1);
+   }
+}
